ac375fb8a7aec44956744d24ef9b42a5f283f3fc
binijaaa <sabine.kolesnikova@gmail.com>
2023-05-11 17:30:43 +0300

    new version with almost done project

--- Meal.cs
+++ Meal.cs
@@ -6,23 +6,54 @@ using System.Threading.Tasks;
 
 namespace RandomMealGenerator
 {
-    internal class Meal
+    public class Rootobject
+
     {
-        private string name;
-        private string area;
-        //private string[] ingredience;
 
-        public string Name { get; set; }
-        public string Area { get; set; }
-        public string Ingredients { get; set; }
-        public string Recipe { get; set; }
+        public string q { get; set; }
+        public int from { get; set; }
+        public int to { get; set; }
+        public bool more { get; set; }
+        public int count { get; set; }
+        public Hit[] hits { get; set; }
 
-        public Meal(string name, string area, string ingredients, string recipe)
+    }
+
+    public class Hit
+
+    {
+        public Recipe recipe { get; set; }
+    }
+
+
+
+    public class Recipe
+
+    {
+        public string label { get; set; }
+        public string source { get; set; }
+        public string url { get; set; }
+        public string[] ingredientLines { get; set; }
+        public Ingredient[] ingredients { get; set; }
+        public float calories { get; set; }
+        public float totalWeight { get; set; }
+        public float totalTime { get; set; }
+        public string[] cuisineType { get; set; }
+        public string[] mealType { get; set; }
+        public string[] dishType { get; set; }
+
+
+        public class Ingredient
+
         {
-            this.Name = name;
-            this.Area = area;
-            this.Ingredients = ingredients;
-            this.Recipe = recipe;
+            public string text { get; set; }
+            public float quantity { get; set; }
+            public string measure { get; set; }
+            public string food { get; set; }
+            public float weight { get; set; }
+            public string foodCategory { get; set; }
+            public string foodId { get; set; }
+          
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -10,51 +10,82 @@ namespace RandomMealGenerator
     {
         static void Main(string[] args)
         {
-            Meal test = new Meal("Pizza","Italy", "something, something, ...", "Recepe description");
-            Console.WriteLine($"Testing: {test.Name}" );
+            //Meal test = new Meal("Pizza","Italy", "something, something, ...", "Recepe description");
+            //Console.WriteLine($"Testing: {test.Name}" );
 
             Console.WriteLine("Choose a country: ");
             string country = Console.ReadLine();
 
             //string testing = RandomizeMealByCountry(country);
 
-            Randomize(country);
+            //Randomize(country);
             //Console.WriteLine(country);
 
 
 
 
             HttpClient client = new HttpClient();
-            string response = client.GetStringAsync($"https://api.spoonacular.com/recipes/random?tags={country}&apiKey=7fcf8633f41b49f1ba173b54e7f6504b").Result;
+            string response = client.GetStringAsync($"https://api.edamam.com/search?app_id=38c4fcc6&app_key=024de75c4492cec0c489343537b5e7ca&from=0&to=10&q={country}").Result; //0 to 10 - the amount of recipes for randomizing
             
-            XXXX root = JsonConvert.DeserializeObject<XXXX>(response); //need to make a new class.cs with all the info from API in json
-            
+            Rootobject ApiResultObject = JsonConvert.DeserializeObject<Rootobject>(response);
+            if(ApiResultObject.hits.Length > 0 )
+            {
+                var recipe = ApiResultObject.hits[GetRandomNumber(ApiResultObject.count)].recipe;
 
-            Console.WriteLine();
+                Console.WriteLine($"Recipe label: {recipe.label}"); //can put whatever info from meal.cs (gotten from API)
+                Console.WriteLine($"Country: {country}");
+                Console.WriteLine($"Recipe ingredients:");
+                foreach(var ingredient in recipe.ingredientLines)//using foreach since the ingredients are in a list
+                {
+                    Console.WriteLine($"  {ingredient}");
+                }
+                Console.WriteLine($"Total calories: {recipe.calories}");
+                Console.WriteLine($"Source: {recipe.source} | {recipe.url}");
+            }
+            else //gives if recipe count is 0
+            {
+                Console.WriteLine("sorry, no recipes found!");
+            }
         }
 
-        static void Randomize(string country)
+        static int GetRandomNumber(int maxRecipeCount) //gives a random recipe from 0-10 found hits (specified in API URL)
         {
+            int maxValue = 10;
+            if(maxRecipeCount < maxValue) //if recipe count is lower than chosen 10
+            {
+                maxValue = maxRecipeCount+1; // +1 because the random.next function gives max excluding nr 10
+            }
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, maxValue); // generates a random integer between 0 and 9 (inclusive)
+            return randomNumber;
+        }
             
-            Console.WriteLine("I am here");
 
 
-        }
 
-        public string RandomizeMealByCountry(string country)
-        {
-            return "yes";
-        }
+        //static void Randomize(string country)
+        //{
+            
+           // Console.WriteLine("I am here");
+
+
+        //}
+
+        //public string RandomizeMealByCountry(string country)
+        //{
+        //    return "yes";
+        //}
 
        
         //int allMeals = TotalMeals();
        // int randomMealNr = RandomizeMeal(allMeals);
 
-        static int RandomizeMeal(int allMeals)
-        {
-            Random random = new Random();
-            return random.Next(1, allMeals + 1);
-        }
+        //static int RandomizeMeal(int allMeals)
+        //{
+        //    Random random = new Random();
+        //    return random.Next(1, allMeals + 1);
+        //}
         
     }
 }
